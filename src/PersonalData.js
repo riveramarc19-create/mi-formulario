f84dcec52a42048e718747acd58755a40bbabdaf
Marco <riveramarc19@gmail.com>
2025-12-25 19:37:31 -0500

    actualizacion de ventanas

--- src/PersonalData.js
+++ src/PersonalData.js
@@ -4,7 +4,7 @@
 // Archivo: PersonalData.js
 export const PERSONAL_LIST = [
   // El campo 'password' es la contraseña que TÚ le creas al usuario.
-  { dni: "70582012", nombre: "RIVERA QUISPE PETER", password: "Pedro123" },
+  { dni: "70582012", nombre: "RIVERA QUISPE PETER", password: "123" },
   { dni: "80907050", nombre: "GONZALES CASTRO JOEHCHE", password: "admin" },
   { dni: "45202010", nombre: "VILELA ALVA JEHISON ANDRE", password: "his" },
   { dni: "12345678", nombre: "USUARIO PRUEBA", password: "000" }
