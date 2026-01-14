7077a4541e94d2399d6ed03661ff0a33f7a21f77
Marco <riveramarc19@gmail.com>
2026-01-14 10:51:44 -0500

    hoy

--- src/PersonalData.js
+++ src/PersonalData.js
@@ -5,6 +5,8 @@
 export const PERSONAL_LIST = [
 
   { dni: "40958276", nombre: "TRONCOS DIAS GABY LIZETH", password: "G2026", ups: "OBSTETRICIA"},
+ { dni: "41918140", nombre: "LOPEZ JULIAN JUDITH JANEET", password: "J2026", ups: "OBSTETRICIA"},
+{ dni: "73888332", nombre: "CHAVEZ CASTRO CATHERINE LOURDES", password: "XHANDER", ups: "OBSTETRICIA"},
   { dni: "02860100", nombre: "CORDOVA MAZA ENOE", password: "1234", ups: "MEDICINA" },
   { dni: "123", nombre: "MARCO", password: "123", ups: "ENFERMERIA" },
 
