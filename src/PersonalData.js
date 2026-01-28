84184d3d468628b867b39b218936955099d409cf
Marco <riveramarc19@gmail.com>
2026-01-28 09:49:35 -0500

    Hoy

--- src/PersonalData.js
+++ src/PersonalData.js
@@ -9,6 +9,7 @@ export const PERSONAL_LIST = [
 { dni: "73888332", nombre: "CHAVEZ CASTRO CATHERINE LOURDES", password: "XHANDER", ups: "OBSTETRICIA"},
 { dni: "47544223", nombre: "QUEREVALU REFORME FIORELLA YADIRA", password: "DEREEK080620", ups: "OBSTETRICIA"},
 { dni: "77810382", nombre: "PIMENTEL CARDOZA ROXALA MILAGROS", password: "222606", ups: "NUTRICION"},
+{ dni: "48245394", nombre: "CHAVEZ HUAMAN ARACELLY", password: "1234", ups: "NUTRICION"},
 { dni: "02860100", nombre: "CORDOVA MAZA ENOE", password: "1234", ups: "MEDICINA" },
 { dni: "123", nombre: "MARCO", password: "012", ups: "ENFERMERIA" },
 
