438785888adbac3149450e655db2f59fa6398233
Marco <riveramarc19@gmail.com>
2026-04-09 18:17:12 -0500

    HOY

--- src/PersonalData.js
+++ src/PersonalData.js
@@ -8,7 +8,7 @@ export const PERSONAL_LIST = [
 { dni: "40958276", nombre: "TRONCOS DIAS GABY LIZETH", password: "G2026", ups: "OBSTETRICIA", establecimiento: "E.S I-4 PACAIPAMPA"},
 { dni: "41918140", nombre: "LOPEZ JULIAN JUDITH JANEET", password: "J2026", ups: "OBSTETRICIA", establecimiento: "E.S I-4 PACAIPAMPA"},//50 EL 05/02/2026 (M)
 { dni: "73888332", nombre: "CHAVEZ CASTRO CATHERINE LOURDES", password: "XHANDER", ups: "OBSTETRICIA", establecimiento: "E.S I-4 PACAIPAMPA"},//50 28/01/2026 (M)
-{ dni: "47544223", nombre: "QUEREVALU REFORME FIORELLA YADIRA", password: "DEREEK080620", ups: "OBSTETRICIA", establecimiento: "E.S I-4 PACAIPAMPA"},//14/01/2026
+{ dni: "47544223", nombre: "QUEREVALU REFORME FIORELLA YADIRA", password: "DEREEK08062014", ups: "OBSTETRICIA", establecimiento: "E.S I-4 PACAIPAMPA"},//14/01/2026
 { dni: "77810382", nombre: "PIMENTEL CARDOZA ROXALA MILAGROS", password: "222606", ups: "NUTRICION", establecimiento: "E.S I-4 PACAIPAMPA"},//100 EL 28/01/2026 (E)
 { dni: "76307612", nombre: "CASTILLO CAUCHA LUDUVINDA", password: "LIAM20", ups: "OBSTETRICIA", establecimiento: "P.S I-1 LAGUNAS DE SAN PABLO"}, //200 EL 02/02/2026 (E,M)
 { dni: "48014885", nombre: "PARDO ANCAJIMA YEFRI EDINSON", password: "Ypardo0804", ups: "OBSTETRICIA", establecimiento: "E.S I-4 PACAIPAMPA"},//100 EL 02/02/26(E)
